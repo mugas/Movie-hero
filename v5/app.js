f85c7f7df57b3c0fbf3635c740e9860478725746
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-08-08 00:20:33 +0300

    more animations

--- v5/app.js
+++ v5/app.js
@@ -10,7 +10,7 @@ for (var i=0; i<roll.length; i++){
 }
     
 
-
+//Image on click
 
 document.querySelector('.first-panel').addEventListener('click', batman);
 document.querySelector('.second-panel').addEventListener('click', bane,);
@@ -22,27 +22,37 @@ document.querySelector('.forth-panel').addEventListener('click', alfred);
 function batman(){
         document.querySelector('.image-1-panel').classList.toggle('notactive');
         document.querySelector('.image-2-panel').classList.toggle('notactive');
-        //document.body.style.background = "red";
-        document.querySelector('.background').classList.toggle('bane-background');
-        
-        
+        document.querySelector('.background').classList.toggle('batman-background');
+        var text = document.getElementById("heroes");
+        if(text.innerHTML === "Heroes are back"){
+            text.innerHTML = "Batman";
+        } else {
+            text.innerHTML = "Heroes are back";
+        }   
     }
 
 function bane(){
     document.querySelector('.image-3-panel').classList.toggle('notactive');
     document.querySelector('.image-4-panel').classList.toggle('notactive');
-    document.body.style.background = "yellow";
-
+    document.querySelector('.background').classList.toggle('bane-background');
+    var text = document.getElementById("heroes");
+    if(text.innerHTML === "Heroes are back"){
+        text.innerHTML="Bane";
+    } else {
+        text.innerHTML = "Heroes are back";
+    }
 }
 
 function joker(){
     document.querySelector('.image-5-panel').classList.toggle('notactive');
     document.querySelector('.image-6-panel').classList.toggle('notactive');
+    document.querySelector('.background').classList.toggle('joker-background');
 }
 
 function alfred(){
     document.querySelector('.image-7-panel').classList.toggle('notactive');
     document.querySelector('.image-8-panel').classList.toggle('notactive');
+    document.querySelector('.background').classList.toggle('alfred-background');
 }
 
 
