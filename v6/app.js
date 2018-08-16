95ecfc8c538c7879168075b557d9c05d5be18e75
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-08-16 23:48:43 +0300

    add readme file and finnish project

--- v6/app.js
+++ v6/app.js
@@ -11,7 +11,7 @@ for (var i=0; i<roll.length; i++){
 }
     
 
-//Image on click
+//Code for every Image on click
 
 document.querySelector('.first-panel').addEventListener('click', batman);
 document.querySelector('.second-panel').addEventListener('click', bane,);
@@ -26,7 +26,7 @@ function batman(){
         document.querySelector('.image-2-panel').classList.toggle('notactive');
         document.querySelector('.background').classList.toggle('batman-background');
         var text = document.getElementById("heroes");
-        if(text.innerHTML === "Heroes are back  "){
+        if(text.innerHTML !== "Batman"){
             text.innerHTML="Batman";
     } else {
             text.innerHTML = "Heroes are back";
@@ -40,7 +40,9 @@ function bane(){
     var text = document.getElementById("heroes");
     if(text.innerHTML !== "Bane"){
         text.innerHTML="Bane";
-} 
+}  else {
+    text.innerHTML = "Heroes are back";
+}  
 
 }
 
@@ -50,12 +52,11 @@ function joker(){
     document.querySelector('.image-6-panel').classList.toggle('notactive');
     document.querySelector('.background').classList.toggle('joker-background');
     var text = document.getElementById("heroes");
-    if(text.innerHTML !== "Joker" && img.src !=="flash.svg"){
-        text.innerHTML = "Joker"; 
-         img.src = "flash.svg";
-    } else {
-        text.innerHTML = "Heroes are back";
-    }
+    if(text.innerHTML !== "Joker"){
+        text.innerHTML="Joker";
+}  else {
+    text.innerHTML = "Heroes are back";
+}  
 }
 
 function alfred(){
@@ -68,7 +69,17 @@ function alfred(){
     } else {
         text.innerHTML = "Heroes are back";
     }
+   
 }
+
+//Set title back to "Heroes are back"
+setInterval(function (){
+    var titles = document.getElementById("heroes");
+    if(titles.innerHTML !=="Heroes are back"){
+        titles.innerHTML ="Heroes are back";
+    }
+    
+},3000);
 
 
 
