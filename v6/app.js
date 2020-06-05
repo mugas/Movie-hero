84a51e70b0a14d633745378693fd61846c055e92
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-06-05 10:08:56 +0300

    logic of the flip in js

--- v6/app.js
+++ v6/app.js
@@ -1,35 +1,20 @@
 const cards = document.querySelectorAll(".cards__single");
 const images = document.querySelectorAll(".cards__image");
 const cardsBack = document.querySelectorAll(".cards__back");
+const cardsFront = document.querySelectorAll(".cards__front");
 
-// console.log(cards);
-// console.log(cardsBack);
-/* const showText = () => {
-  console.log("hello");
-  // images.classList.add("cards__opacity");
-  cards.classList.toggle("flipped");
-}; */
-
-const showBackCard = () => {
-  const test = cards.classList.add(test);
-  console.log("hello");
-  console.log(cardsBack);
-  console.log(images);
-  console.log(cards.classList);
-
-  // cardsBack.classList.add("cards__show");
-  // show text from classlist cardsback
-  // this.card.classList.add("cards__show");
-};
-
-cards.forEach((card) => {
-  card.addEventListener("click", showBackCard);
-});
 /* cards.forEach((card) => {
-  card.addEventListener("click", showText);
+  card.addEventListener("click", () => {
+    console.log(cards.classList);
+    if (card.classList.contains("test")) {
+      card.classList.remove("test");
+    } else {
+      card.classList.add("test");
+    }
+  });
 }); */
 
-// cards.addEventListener("click", showText);
-/* images.forEach((image) => {
-  image.addEventListener("click", showText);
-}); */
+function flipCard() {
+  this.classList.toggle("test");
+}
+cards.forEach((card) => card.addEventListener("click", flipCard));
