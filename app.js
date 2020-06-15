be7f7a3a5833ec6e0495a1e7107f4ad647484b2f
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-06-15 07:23:07 +0300

    finish files

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const cards = document.querySelectorAll(".cards__single");
 
 function flipCard() {
   this.classList.toggle("flip");
-  this.classList.toggle("test");
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
