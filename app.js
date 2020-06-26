491b336a462a6dc52fe406eda27384f0d8485b05
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2020-06-26 07:50:57 +0300

    last changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const cards = document.querySelectorAll(".cards__single");
 
 function flipCard() {
+  // toggle the cards on click
   this.classList.toggle("flip");
 }
+
 cards.forEach((card) => card.addEventListener("click", flipCard));
